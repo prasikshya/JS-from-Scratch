06db21d7cd71d9e61d3413aa821365b9aee544ac
prasikshya <prasikshya.222baral@gmail.com>
2023-12-14 07:01:37 +0545

    Day1

---
+++ conversion.js
@@ -0,0 +1,15 @@
+// let age=window.prompt("how old are you?");
+// age=Number(age);
+// age+=1;
+// console.log("Happy Birthday! you are",age,"years old");
+
+//string conversion:
+let x;
+let y;
+let z;
+x=Number("3.14");
+y=String(3.14);
+z=Boolean("hehe");
+console.log(x,typeof x);
+console.log(y,typeof y);
+console.log(z,typeof z);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+let age=window.prompt("how old are you?");
+// age=Number(age);
+// age+=1;
+// console.log("Happy Birthday! you are",age,"years old");
