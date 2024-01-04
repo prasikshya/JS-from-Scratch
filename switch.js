73fcdf8bb35291c7f850b9788ed9a6d05bc19180
prasikshya <prasikshya.222baral@gmail.com>
2024-01-04 07:18:25 +0545

    day 5

---
+++ switch.js
@@ -0,0 +1,16 @@
+const myBtn = document.getElementById("myBtn");
+const text = document.getElementById("text");
+
+myBtn.onclick = function() {
+    let para=paragraph.value;
+    let channel = text.value;
+
+    switch (channel) {
+        case "messi":
+            console.log("congratulations you understand football <33 ");
+            break;
+        default:
+            console.log("hey!you're in dilemaa..enter the real GOAT");
+    }
+
+};
