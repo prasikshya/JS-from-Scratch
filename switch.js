3010542118cd56cb10f79056cbe384cfa0b2732f
prasikshya <prasikshya.222baral@gmail.com>
2024-01-05 16:51:03 +0545

    Day 6

--- switch.js
+++ switch.js
@@ -1,16 +1,38 @@
-const myBtn = document.getElementById("myBtn");
-const text = document.getElementById("text");
+// const myBtn = document.getElementById("myBtn");
+// const text = document.getElementById("text");
 
-myBtn.onclick = function() {
-    let para=paragraph.value;
-    let channel = text.value;
+// myBtn.onclick = function() {
+//     let para=paragraph.value;
+//     let channel = text.value;
 
-    switch (channel) {
-        case "messi":
-            console.log("congratulations you understand football <33 ");
-            break;
-        default:
-            console.log("hey!you're in dilemaa..enter the real GOAT");
-    }
+//     switch (channel) {
+//         case "messi":
+//             console.log("congratulations you understand football <33 ");
+//             break;
+//         default:
+//             console.log("hey!you're in dilemaa..enter the real GOAT");
+//     }
 
-};
+// };
+
+let testGrade = 90;
+let lettergrade;
+
+switch (true) {
+    case testGrade >= 90:
+        lettergrade = "A";
+        break;
+    case testGrade >= 80:
+        lettergrade = "B+";
+        break;
+    case testGrade >= 70:
+        lettergrade = "B";
+        break;
+    case testGrade >= 60:
+        lettergrade = "C";
+        break;
+    default:
+        lettergrade = "F";
+}
+
+console.log(lettergrade);
