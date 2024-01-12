d712387cf139563e0ead7e9375ee243cba0b8058
prasikshya <prasikshya.222baral@gmail.com>
2024-01-12 20:10:51 +0545

    Day 10

---
+++ map.js
@@ -0,0 +1,13 @@
+const numbers=[1,2,3,4,5];
+const squares=numbers.map(square);
+const cubes=numbers.map(cube);
+console.log(`cube of given element is ${cubes}`);
+console.log(`square of given element is ${squares}`);
+
+
+function square(element){
+    return Math.pow(element,2);
+}
+function cube(element){
+    return Math.pow(element,3);
+}
