52af67d81e72b72f47a9d82fb319db4d047e247a
prasikshya <prasikshya.222baral@gmail.com>
2024-01-12 20:20:53 +0545

    Day 10

--- map.js
+++ map.js
@@ -1,13 +1,22 @@
-const numbers=[1,2,3,4,5];
-const squares=numbers.map(square);
-const cubes=numbers.map(cube);
-console.log(`cube of given element is ${cubes}`);
-console.log(`square of given element is ${squares}`);
+// const numbers=[1,2,3,4,5];
+// const squares=numbers.map(square);
+// const cubes=numbers.map(cube);
+// console.log(`cube of given element is ${cubes}`);
+// console.log(`square of given element is ${squares}`);
 
 
-function square(element){
-    return Math.pow(element,2);
-}
-function cube(element){
-    return Math.pow(element,3);
+// function square(element){
+//     return Math.pow(element,2);
+// }
+// function cube(element){
+//     return Math.pow(element,3);
+// }
+
+const students=["motu","patlu","tom","jerry"];
+const studentsUpper=students.map(uppercase);
+
+console.log(studentsUpper);
+
+function uppercase(element){
+    return element.toUpperCase();
 }
