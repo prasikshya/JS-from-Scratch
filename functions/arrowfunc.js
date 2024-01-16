2ef255966ab4f0d7a031a67b0aadb046e39bab72
prasikshya <prasikshya.222baral@gmail.com>
2024-01-16 22:02:01 +0545

    day 12

---
+++ functions/arrowfunc.js
@@ -0,0 +1,10 @@
+// arrow func--> a consise way to write function expressions 
+// good for simple functions that we use only once
+
+// const hello=(name,age)=>{console.log(`hello ${name}`);
+// console.log(`age of ${name } is :${age}`);
+
+// }
+// hello("noone",100);
+
+setTimeout(() => console.log("hello"),3000);
