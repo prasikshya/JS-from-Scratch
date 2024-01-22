d9924e2cd1eb85d3732ae4c6cbbc3a84daa6f006
prasikshya <prasikshya.222baral@gmail.com>
2024-01-22 20:20:11 +0545

    Day 18

---
+++ dateobj.js
@@ -0,0 +1,29 @@
+
+
+// const date=new Date();
+// const year=date.getFullYear();
+// const month=date.getMonth();
+// const day=date.getDate();
+// const hour=date.getHours();
+// const dayOfweek=date.getDay();
+
+// console.log(year);
+// console.log(month);
+// console.log(day);
+// console.log(hour);
+// console.log(dayOfweek);
+
+// const date=new Date();
+// date.setFullYear(2024);
+// date.setMonth(0);
+// date.setDate(1);
+// date.setMinutes(3);
+
+// console.log(date);
+
+const date1=new Date("2023-12-31");
+const date2=new Date("2024-01-01");
+
+if(date2 >date1){
+    console.log("happy new year !!");
+}
