6a1af348cfe9ca3e2bd1e5da361deb88fcfd571a
prasikshya <prasikshya.222baral@gmail.com>
2024-01-25 08:01:42 +0545

    Day 21

---
+++ moduletry/mod.js
@@ -0,0 +1,6 @@
+// yourFile.js
+import { PI, getCircumference, getArea, getVolume } from './mathUtil.js';
+
+console.log(PI);
+const circumference = getCircumference(12);
+console.log(`${circumference.toFixed(2)} cm^2`);
