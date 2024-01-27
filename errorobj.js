420eeb67336c413d9d68122860aa46a688840548
prasikshya <prasikshya.222baral@gmail.com>
2024-01-27 10:31:35 +0545

    Day 23

--- errorobj.js
+++ errorobj.js
@@ -14,8 +14,14 @@
 
 
 try {
-    const dividend = window.prompt(`enter the dividend`);
-    const divisor = window.prompt(`enter the divisor`);
+    const dividend = Number(window.prompt(`enter the dividend`));
+    const divisor = Number(window.prompt(`enter the divisor`));
+    if (divisor==0){
+        throw new Error("You can't divide by zero");
+    }
+    if (isNaN(dividend)|| isNaN(divisor)){
+        throw new Error("Values must be a number");
+    }
     const result = dividend / divisor;
     console.log(result);
 }
